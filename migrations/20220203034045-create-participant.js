9ce77fd64cc6b1469f4816fe3da62dd855cdad05
hasbyadam <hasbyadam@gmail.com>
2022-02-03 10:41:58 +0700

    + models

---
+++ migrations/20220203034045-create-participant.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Participants', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      arisanId: {
+        type: Sequelize.INTEGER
+      },
+      havePaid: {
+        type: Sequelize.BOOLEAN
+      },
+      haveWon: {
+        type: Sequelize.BOOLEAN
+      },
+      isAdmin: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Participants');
+  }
+};
---
+++ models/arisan.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Arisan extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Arisan.init({
+    title: DataTypes.STRING,
+    dues: DataTypes.STRING,
+    paymentPeriod: DataTypes.STRING,
+    lotteryDate: DataTypes.DATE,
+    balance: DataTypes.INTEGER,
+    totalParticipant: DataTypes.INTEGER,
+    status: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Arisan',
+  });
+  return Arisan;
+};
