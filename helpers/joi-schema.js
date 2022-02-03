75b1487812a2f766a012f21100e48e9ccc95ec5f
hasbyadam <hasbyadam@gmail.com>
2022-02-03 10:12:32 +0700

    + middleware auth & validator

---
+++ helpers/joi-schema.js
@@ -0,0 +1,16 @@
+const Joi = require("joi").extend(require("@joi/date"));
+
+module.exports = {
+  registerSchema: Joi.object({
+    first_name: Joi.string().required(),
+    last_name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string()
+      .min(5)
+      .regex(/^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[a-z])/)
+      .message(
+        '"password" should contain a mix of uppercase and lowercase letters, numbers, and special characters ',
+      )
+      .required(),
+  }),
+};
---
+++ middlewares/auth.js
@@ -0,0 +1,43 @@
+const { Users } = require("../models");
+const jwt = require("jsonwebtoken");
+const catchError = require("../utils/error");
+
+module.exports = {
+  isLogin: async (req, res, next) => {
+    try {
+      let token = req.header("Authorization");
+      if (!token) {
+        return res.status(401).json({
+          status: "Unauthorized",
+          message: "No token detected",
+        });
+      }
+      token = token.replace("Bearer ", "");
+      jwt.verify(token, process.env.SECRET_TOKEN, async (error, decoded) => {
+        if (error)
+          return res.status(401).json({
+            status: "Unauthorized",
+            message: "Invalid access token",
+          });
+        const user = await Users.findOne({
+          where: {
+            id: decoded.id,
+          },
+        });
+        if (!user) {
+          return res.status(401).json({
+            status: "Unauthorized",
+            message: "User not found",
+          });
+        }
+        req.user = {
+          id: decoded.id,
+          email: decoded.email,
+        };
+        next();
+      });
+    } catch (error) {
+      catchError(error, res);
+    }
+  },
+};
