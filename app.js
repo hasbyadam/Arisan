f44b0e2a97c43679549f6d0c321ff759429dffd0
hasbyadam <hasbyadam@gmail.com>
2022-02-07 14:55:54 +0700

    fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("./startup")(app);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('Connected')
