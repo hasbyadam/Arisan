20d7798dc472e719c5a22d90b6e57c61260563c1
hasbyadam <hasbyadam@gmail.com>
2022-02-12 11:57:08 +0700

    +CRUD Participant

--- controllers/participantController.js
+++ controllers/participantController.js
@@ -1,34 +1,98 @@
 const catchError = require("../utils/error");
-const { Participant, Contact, User } = require('../models')
+const { Participant, Contact, User } = require("../models");
 
 module.exports = {
-    create: async (req, res) => {
-        try {
-            const { contactId } = req.body
-            const users = []
-            const contact = await Contact.findAll({
-                where: { userId: req.user.id },
-            })
-            for (let i = 0; i < contact.length; i++) {
-                const data = await Contact.findOne({
-                    where: { userId: req.user.id, id: contactId[i] },
-                })
-                const search = await User.findOne({
-                    where: { phoneNumber: data.dataValues.phoneNumber }
-                })
-                const user = { userId: search.dataValues.id, arisanId: req.params.arisanId, haveWon: false, havePaid:false }
-                users.push(user)
-            }
-            const participants = await Participant.bulkCreate(users)
-            res.status(200).json({
-                status: "Success",
-                message: "Successfully to create participant",
-                result: participants,
-              });
-        } catch (error) {
-            catchError(error,res)
-        }
+  create: async (req, res) => {
+    try {
+      const { contactId } = req.body;
+      const users = [];
+      const contact = await Contact.findAll({
+        where: { userId: req.user.id },
+      });
+      for (let i = 0; i < contact.length; i++) {
+        const data = await Contact.findOne({
+          where: { userId: req.user.id, id: contactId[i] },
+        });
+        const search = await User.findOne({
+          where: { phoneNumber: data.dataValues.phoneNumber },
+        });
+        const user = {
+          userId: search.dataValues.id,
+          arisanId: req.params.arisanId,
+          haveWon: false,
+          havePaid: false,
+        };
+        users.push(user);
+      }
+      const participants = await Participant.bulkCreate(users);
+      res.status(200).json({
+        status: "Success",
+        message: "Successfully to create participant",
+        result: participants,
+      });
+    } catch (error) {
+      catchError(error, res);
     }
-
-}
-
+  },
+  edit: async (req, res) => {
+      try {
+        const { havePaid } = req.body
+        await Participant.update(
+            {
+              havePaid: havePaid,
+            },
+            { where: { id: req.params.participantId } }
+          );
+          res.status(200).json({
+            status: "Success",
+            message: "participant filtered",
+            result: {},
+          });
+      } catch (error) {
+        catchError(error, res);
+    }
+  },
+  filter: async (req, res) => {
+    try {
+      const { havePaid } = req.body;
+      const data = await Participant.findAll({
+        where: { havePaid: havePaid },
+      });
+      res.status(200).json({
+        status: "Success",
+        message: "participant filtered",
+        result: data,
+      });
+    } catch (error) {
+      catchError(error, res);
+    }
+    },
+    remove: async (req, res) => {
+        try {
+         await Participant.destroy({
+            where: { id: req.params.participantId },
+          });
+          res.status(200).json({
+            status: "Success",
+            message: "participant deleted",
+            result: {},
+          });
+        } catch (error) {
+          catchError(error, res);
+        }
+    },
+    fetchAll: async (req, res) => {
+        try {
+          const data = await Participant.findAll({
+            where: { arisanId: req.params.arisanId },
+          });
+          res.status(200).json({
+            status: "Success",
+            message: "participant fetched",
+            result: data,
+          });
+        } catch (error) {
+          catchError(error, res);
+        }
+        },
+};
--- routes/participantRoute.js
+++ routes/participantRoute.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { create } = require('../controllers/participantController')
+const { create, edit, remove, filter, fetchAll } = require('../controllers/participantController')
 const {isLogin} = require('../middlewares/auth')
 
 
 router.post("/create/:arisanId", isLogin, create);
+router.put("/edit/:participantId", isLogin, edit);
+router.delete("/delete/:participantId", isLogin, remove);
+router.get("/filter/", isLogin, filter);
+router.get("/:arisanId", isLogin, filter, fetchAll);
 
 
 module.exports = router
